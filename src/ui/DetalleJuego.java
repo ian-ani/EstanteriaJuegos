663324ac26889ef5679c5ecd6ef1239d69558d41
Iris <irisaunav@gmail.com>
2026-04-26 18:01:16 +0200

    Add ui classes

---
+++ src/ui/DetalleJuego.java
@@ -0,0 +1,224 @@
+package ui;
+
+import modelo.Estado;
+import modelo.Valoracion;
+import utiles.Mensaje;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+
+public class DetalleJuego extends JDialog {
+    private Set<String> etiquetasTmp = new HashSet<>();
+
+    private JPanel panelGeneral;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JPanel botonesPanel;
+    private JPanel botonesPanelSecundario;
+    private JPanel detallePanel;
+    private JLabel nombreLabel;
+    private JLabel plataformaLabel;
+    private JLabel estadoLabel;
+    private JLabel etiquetasLabel;
+    private JLabel valoracionLabel;
+    private JLabel notasLabel;
+    private JTextField nombreEntrada;
+    private JTextField plataformaEntrada;
+    private JRadioButton completadoRadio;
+    private JRadioButton jugandoRadio;
+    private JRadioButton pendienteRadio;
+    private JRadioButton abandonadoRadio;
+    private JPanel nombrePanel;
+    private JPanel plataformaPanel;
+    private JPanel estadoPanel;
+    private JPanel etiquetasPanel;
+    private JPanel valoracionPanel;
+    private JRadioButton gustadoRadio;
+    private JRadioButton indiferenteRadio;
+    private JRadioButton noGustadoRadio;
+    private JPanel notasPanel;
+    private JTextArea notasEntrada;
+    private JRadioButton noValoradoRadio;
+    private JPanel valoracionEstadoPanel;
+    private JTextField etiquetaEntrada;
+    private JButton anadirEtiquetaButton;
+    private JPanel etiquetasAnadidasPanel;
+
+    public DetalleJuego() {
+        setContentPane(panelGeneral);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        panelGeneral.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+        // Evento al anadir una etiqueta y pulsar '+'
+        anadirEtiquetaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!validarLargoEtiquetas()) {
+                    if (validarEntrada(15, etiquetaEntrada)) {
+                        // TODO hacer que los botones de las etiquetas sean mas pequenos, se ve feo
+                        // Anadir panel con campo de texto y un boton de eliminar
+                        crearPanelEtiqueta();
+                    } else {
+                        Mensaje.mostrarMensajeError(
+                                "Etiqueta no válida",
+                                "La etiqueta no puede estar vacía ni contener más de 15 caracteres."
+                        );
+                        return;
+                    }
+                } else {
+                    Mensaje.mostrarMensajeError("Límite etiquetas", "No se pueden añadir más de 3 etiquetas.");
+                    return;
+                }
+
+                // Redibujar panel
+                redibujarEtiquetas(etiquetasAnadidasPanel);
+            }
+        });
+    }
+
+    private void redibujarEtiquetas(JPanel panel) {
+        panel.revalidate();
+        panel.repaint();
+    }
+
+    private void crearPanelEtiqueta() {
+        // Anadir panel
+        JPanel panel = new JPanel();
+        etiquetasAnadidasPanel.add(panel);
+
+        // Anadir texto
+        panel.add(new JTextField(etiquetaEntrada.getText().trim()));
+
+        // Anadir boton
+        JButton btn = new JButton("x");
+        panel.add(btn);
+
+        // Evento que borra un panel (con su campo de texto y boton)
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                etiquetasAnadidasPanel.remove(panel);
+
+                // Redibujar panel
+                redibujarEtiquetas(etiquetasAnadidasPanel);
+            }
+        });
+    }
+
+    private boolean validarLargoEtiquetas() {
+        // Numero de etiquetas
+        int numEtiquetas = 0;
+
+        // Componentes del panel
+        Component[] componentes = etiquetasAnadidasPanel.getComponents();
+
+        // Recorrer componentes, si es un JPanel incrementar el contador
+        for (Component c: componentes) {
+            if (c instanceof JPanel) {
+                numEtiquetas++;
+            }
+        }
+
+        return (numEtiquetas >= 3);
+    }
+
+    // Valida el largo de las entradas
+    private boolean validarEntrada(int largo, JTextField campo) {
+        if (campo.getText().trim().isEmpty()) {
+            return false;
+        }
+
+        return campo.getText().trim().length() <= largo;
+    }
+
+    // Obtener valor de los radios
+    private JRadioButton getRadio(JPanel panel) {
+        Component[] componentes = panel.getComponents();
+
+        for (Component c: componentes) {
+            if (c instanceof JRadioButton btn) {
+                if (btn.isSelected()) {
+                    return btn;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    // Guardar los datos de campos y radios en Juego
+    private void guardarJuego() {
+        String nombre = nombreEntrada.getText();
+        String plataforma = plataformaEntrada.getText();
+        Estado estado = getRadio(estadoPanel);
+        // TODO etiquetas debe ser un text field con tags/chips
+        Valoracion valoracion = getRadio(valoracionPanel);
+        String notas = notasEntrada.getText();
+
+        // TODO falta guardar
+    }
+
+    // Boton 'OK'
+    private void onOK() {
+        try {
+            guardarJuego();
+            // TODO limpiar panel al anadirlo
+        } catch (Exception e) {
+            Mensaje.mostrarMensajeError("Error", "No se ha podido guardar el juego.");
+        }
+    }
+
+    // Boton 'CANCELAR'
+    private void onCancel() {
+        // Cerrar ventana
+        dispose();
+    }
+
+    // MAIN
+    public static void main(String[] args) {
+        DetalleJuego dialog = new DetalleJuego();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+
+    // Crear y editar podrian llamar ambos a esta ventana, pero desde GestionJuegos
+    // se podria mandar el objeto de ese juego en especifico
+    // Si es null no se ha creado, entonces llama a crear
+    // Si no es null ya existe y se llama a editar
+    // Si es editar se muestra el contenido en sus respectivos textfields, radios, etc
+    // Si se da a OK que de una senal de mostrar toda la tabla de nuevo desde la principal, asi que
+    // habra que pasar el objeto tambien previamente? mas o menos es la idea
+}
---
+++ src/ui/VentanaPrincipal.java
@@ -0,0 +1,121 @@
+package ui;
+
+import modelo.Juego;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import static config.UIConstantes.*;
+
+public class VentanaPrincipal {
+    /* ATRIBUTOS */
+
+    // Ventana secundaria
+    private DetalleJuego detalleJuegoWindow;
+
+    // Variables de GestionJuegos
+    private JPanel panelGeneral;
+    private JLabel tituloLabel;
+    private JPanel tituloPanel;
+    private JPanel busquedaPanel;
+    private JPanel juegoPanel;
+    private JPanel botonesPanel;
+    private JButton eliminarButton;
+    private JButton editarButton;
+    private JButton anadirButton;
+    private JScrollPane tablaPanel;
+    private JTextField busquedaEntrada;
+    private JLabel busquedaLabel;
+    private JTable tabla;
+    private JPanel juegoBotonPanel;
+    private JPanel persistenciaPanel;
+    private JButton Guardar;
+
+    // Lista donde se van a guardar los juegos temporalmente
+    List<Juego> juegos = new ArrayList<>();
+
+    /* CONSTRUCTOR */
+
+    public VentanaPrincipal() {
+        crearTabla();
+        anadirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                anadir();
+            }
+        });
+    }
+
+    /* GETTERS */
+
+    public JPanel getPanelGeneral() {
+        return panelGeneral;
+    }
+
+    /* OTROS METODOS */
+
+    private void crearTabla() {
+        // Matriz de datos para guardar los juegos
+        Object[][] datos = new Object[juegos.size()][6];
+
+        // Fila
+        for (int i = 0; i < datos.length; i++) {
+            // Columna
+            for (int j = 0; j < datos[0].length; j++) {
+                datos[i][0] = juegos.get(i).getNombre();
+                datos[i][1] = juegos.get(i).getPlataforma();
+                datos[i][2] = juegos.get(i).getEstado();
+                datos[i][3] = juegos.get(i).getEtiquetas();
+                datos[i][4] = juegos.get(i).getValoracion();
+                datos[i][5] = juegos.get(i).getNotas();
+            }
+        }
+
+        // Crear tabla: datos y cabecera
+        tabla.setModel(new DefaultTableModel(
+                datos,
+                new String[]{
+                        "Nombre",
+                        "Plataforma",
+                        "Estado",
+                        "Etiquetas",
+                        "Valoración",
+                        "Comentarios",
+                }
+        ));
+    }
+
+    private void anadir() {
+        if (detalleJuegoWindow == null) {
+            detalleJuegoWindow = new DetalleJuego();
+
+            detalleJuegoWindow.setTitle("Añadir juego...");
+
+            detalleJuegoWindow.setSize(kANCHO_VENTANA, kALTO_VENTANA);
+
+            detalleJuegoWindow.setResizable(false);
+
+            detalleJuegoWindow.setLocationRelativeTo(null);
+        }
+
+        detalleJuegoWindow.setVisible(true);
+    }
+
+    private void eliminar() {
+        // Borrar juego seleccionado
+    }
+
+    private void editar() {
+
+    }
+
+    // Guardar juegos en archivo para persistencia!
+    // Permitir exportar? si eso al final
+    // Editar debe abrir una nueva ventana
+    // En principio permitir buscar solo por nombre, luego podria anadir busqueda por mas campos
+    // Seguramente pulsando a algo de la cabecera podria filtrar por asc desc en base a esa columna?
+}
