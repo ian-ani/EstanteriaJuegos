3b174fe2fe9c14a99f68b4e448d48dc012e5c0ae
Iris <irisaunav@gmail.com>
2026-06-25 13:04:45 +0200

    Change table name

--- src/util/GameQueries.java
+++ src/util/GameQueries.java
@@ -2,17 +2,17 @@ package util;
 
 public class GameQueries {
     // CREATE
-    public static final String kINSERT_GAME = "INSERT INTO estanteria_juegos (nombre, plataforma, estado, " +
+    public static final String kINSERT_GAME = "INSERT INTO juegos (nombre, plataforma, estado, " +
             "etiquetas, valoracion, comentarios) VALUES (?, ?, ?, ?, ?, ?)";
 
     // READ
-    public static final String kGET_ALL_GAMES = "SELECT * FROM estanteria_juegos ORDER BY id DESC";
-    public static final String kGET_GAME_BY_NAME = "SELECT * FROM estanteria_juegos WHERE nombre LIKE ?";
+    public static final String kGET_ALL_GAMES = "SELECT * FROM juegos ORDER BY id DESC";
+    public static final String kGET_GAME_BY_NAME = "SELECT * FROM juegos WHERE nombre LIKE ?";
 
     // UPDATE
-    public static final String kUPDATE_GAME = "UPDATE estanteria_juegos SET nombre = ?, plataforma = ?, estado = ?, " +
+    public static final String kUPDATE_GAME = "UPDATE juegos SET nombre = ?, plataforma = ?, estado = ?, " +
             "etiquetas = ?, valoracion = ?, comentarios = ? WHERE id = ?";
 
     // DELETE
-    public static final String kDELETE_GAME = "DELETE FROM estanteria_juegos WHERE id = ?";
+    public static final String kDELETE_GAME = "DELETE FROM juegos WHERE id = ?";
 }
